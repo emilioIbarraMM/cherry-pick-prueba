217c837f6d83cc88a850f76d221de6986727c673
Emilio Ibarra <emilio.ibarra@metricamovil.com>
2025-07-17 19:07:40 +0000

    mensaje 4 añadido, ya terminado

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const mensaje1 = require("./mensaje1");
 const mensaje2 = require("./mensaje2");
 const mensaje3 = require("./mensaje3");
+const mensaje4 = require("./mensaje4");
 
 console.log("Inicio del programa");
 
 mensaje1();
 mensaje2();
 mensaje3();
+mensaje4();
 
 console.log("Fin del programa");
---
+++ mensaje4.js
@@ -0,0 +1,5 @@
+const mensaje4 = () => {
+  console.log("Mensaje 4 terminado");
+};
+
+module.exports = mensaje4;
