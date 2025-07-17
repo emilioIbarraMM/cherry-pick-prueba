80b4093f665b8b85db01ce3c4b1d5871db4c65bb
Emilio Ibarra <emilio.ibarra@metricamovil.com>
2025-07-17 19:07:40 +0000

    confictos solucionados

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const mensaje1 = require("./mensaje1");
 const mensaje2 = require("./mensaje2");
 
+const mensaje4 = require("./mensaje4");
+
 console.log("Inicio del programa");
 
 mensaje1();
 mensaje2();
+
+mensaje4();
 
 console.log("Fin del programa");
